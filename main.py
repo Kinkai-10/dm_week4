f5803fffee674fd6974ef89040f7f7ee7d14cfa8
kinkai <e185710@ie.u-ryukyu.ac.jp>
2020-10-27 17:15:20 +0900

    add test_predict

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ import regression
 model = regression.LinearRegression()
 print(model.x)
 
+#test fit
 import importlib
 importlib.reload(regression)
 model = regression.LinearRegression()
 model.fit(X,Y)
 print(model.theta)
+
+#test predict
+model.predict(X)
+print(model.predict(X))
