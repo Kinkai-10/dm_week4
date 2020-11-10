0173ab5f2a2098502cb4f882dbc910de4dadcd88
kinkai <e185710@ie.u-ryukyu.ac.jp>
2020-11-10 17:23:55 +0900

    add vector_regression

--- main2.py
+++ main2.py
@@ -1,5 +1,12 @@
 import datasets
+import regression
 
 X,Y = datasets.load_nonlinear_example1()
 ex_X = datasets.polynomial2_features(X)
 print(ex_X)
+
+model = regression.LinearRegression()
+model.fit(ex_X, Y)
+print(model.theta)
+print(model.predict(ex_X))
+print(model.score(ex_X, Y))
